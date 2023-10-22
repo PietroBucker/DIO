a9bf83fd19337e21994f23b36924e94701bfee3d
Pietro Bucker <pietro_177@hotmail.com>
2023-10-22 01:27:36 -0300

    subindo pasatas do projetos

--- Santander-Java-Angular/ProjetoControleFluxo/src/App.java
+++ Santander-Java-Angular/ProjetoControleFluxo/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        String teste = "teste";
+        var greeting = STR."Hello \{this.user.firstname()}, how are you?\nIt's \{tempC}°C today!";
+        System.out.println(greeting);
     }
 }
--- Santander-Java-Angular/ProjetoControleFluxo/src/ParametrosInvalidosException.java
+++ Santander-Java-Angular/ProjetoControleFluxo/src/ParametrosInvalidosException.java
@@ -2,4 +2,10 @@ public class ParametrosInvalidosException extends Exception {
     public String getMessage() {
         return "Primeiro numero deve ser menor que o segundo numero";
     }
+
+    @Override
+    public String toString() {
+        return "ParametrosInvalidosException []";
+    }
+   
 }
